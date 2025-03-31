13893ac719e04556a1509d06f02087fac23ee2cb
doanminhduc1007 <ducshooter.luvina@gmail.com>
2025-03-31 21:59:26 +0700

    slider crazy effects by html css js v1

---
+++ app.js
@@ -0,0 +1,39 @@
+let nextDom      = document.getElementById('next');
+let prevDom      = document.getElementById('prev');
+let carouselDOM  = document.querySelector('.carousel');
+let listItemDom  = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+nextDom.onclick = function() {
+  showSlider('next');
+}
+prevDom.onclick = function() {
+  showSlider('prev');
+}
+let timeRunning = 700;
+let timeAutoNext = 7000;
+let runTimeOut;
+clearTimeout(runAutoRun);
+let runAutoRun = setTimeout(() => {
+  nextDom.click();
+}, timeAutoNext);
+function showSlider(type) {
+  let itemSlider = document.querySelectorAll('.carousel .list .item');
+  let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+  if (type === 'next') {
+    listItemDom.appendChild(itemSlider[0]);
+    thumbnailDom.appendChild(itemThumbnail[0]);
+    carouselDOM.classList.add('next');
+  } else {
+    let positionLastItem = itemSlider.length - 1;
+    listItemDom.prepend(itemSlider[positionLastItem]);
+    thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+    carouselDOM.classList.add('prev');
+  }
+
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDOM.classList.remove('next');
+    carouselDOM.classList.remove('prev');
+  }, timeRunning);
+}
